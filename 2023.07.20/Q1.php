16124579b9b590b05965315869e48af631659407
Dasu2000 <135571877+Dasu2000@users.noreply.github.com>
2023-07-23 14:32:49 +0530

    Add files via upload

---
+++ 2023.07.20/Q1.php
@@ -0,0 +1,60 @@
+<html>
+	
+	<body>
+	<table border="1">
+	<tr><td>First name</td><td><?php
+		
+		
+			$a=$_REQUEST["fn"];
+			echo"$a";
+			
+		?></td></tr>
+		<tr><td>Last name</td><td><?php
+		
+		
+			$b=$_REQUEST["ln"];
+			echo" $b";
+			
+		?></td></tr>
+		<tr><td>User name</td><td><?php
+		
+		
+			$c=$_REQUEST["un"];
+			echo" $c";
+			
+		?></td></tr>
+		<tr><td>First name</td><td><?php
+		
+		
+			$d=$_REQUEST["pw"];
+			echo"$d";
+			
+		?></td></tr>
+		<tr><td>Gender</td><td><?php
+		
+		
+			$e=$_REQUEST["gender"];
+			echo" $e";
+			
+		?></td></tr>
+		<tr><td>Email</td><td><?php
+		
+		
+			$f=$_REQUEST["email"];
+			echo"$f";
+			
+		?></td></tr>
+		<tr><td>Phone Nuber</td><td><?php
+		
+		
+			$g=$_REQUEST["pnum"];
+			echo"$g";
+			
+		?></td></tr>
+	
+		
+		
+	</body>
+
+
+</html>
