eae868500d3793c7329a3488180dee6c61864cb4
Dasu2000 <135571877+Dasu2000@users.noreply.github.com>
2023-07-28 00:30:04 +0530

    Add files via upload
    Last tutorial

---
+++ 2023.07.28/Q1-form.php
@@ -0,0 +1,25 @@
+<html>
+	
+	
+	<body>
+	
+	<center>
+	<h1>Login Page</h1>
+		<form name="p" method="post" action="27db.php">
+		
+		Student ID:<input type="text" name="a"><br><br>
+		
+		Student Name:<input type="text" name="b"><br><br>
+		
+		User Name:<input type="text" name="c">
+           
+         <br><br>
+        Password:<input type="password" name="d" >
+		<br><br>
+		<input type="submit" name="g" value="click">
+		</form>
+		</center>
+	</body>
+
+
+</html>
